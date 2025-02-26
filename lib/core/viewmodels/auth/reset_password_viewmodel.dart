3280e3bda2b610fd3754f33e4e240e16712eeacf
cylia03 <cyliakasdi77@gmail.com>
2025-02-26 15:29:28 +0100

    Refactored ResetPasswordViewModel to use BaseViewModel

--- lib/core/viewmodels/auth/reset_password_viewmodel.dart
+++ lib/core/viewmodels/auth/reset_password_viewmodel.dart
@@ -1,37 +1,27 @@
-import 'package:flutter/foundation.dart';
+import 'package:eventura/core/viewmodels/base_viewmodel.dart';
 import 'package:eventura/core/services/auth_service.dart';
 
-class ResetPasswordViewModel extends ChangeNotifier {
+class ResetPasswordViewModel extends BaseViewModel {
   final AuthService _authService;
-  // Constructeur
-  ResetPasswordViewModel({required AuthService authService}) :
-   _authService = authService;
-  //
-  bool _isLoading = false;
-  String? _errorMessage;
 
-  bool get isLoading => _isLoading;
-  String? get errorMessage => _errorMessage;
+  // Constructeur
+  ResetPasswordViewModel({required AuthService authService}) : _authService = authService;
 
   // Méthode pour réinitialiser le mot de passe
   Future<void> resetPassword(String email) async {
-    // On met isLoading à true pour afficher le spinner de chargement
-    _isLoading = true;
-    // On réinitialise le message d'erreur
-    _errorMessage = null;
-    // On notifie les écouteurs du changement
-    notifyListeners();
+    // Active le mode "chargement"
+    setBusy(true);
+    setError(null); // Réinitialise les erreurs
 
     try {
       // Appel de la méthode resetPassword() du service d'authentification
       await _authService.resetPassword(email);
       print("Réinitialisation du mot de passe demandée avec succès.");
     } catch (error) {
-      _errorMessage = "Une erreur s'est produite lors de la réinitialisation du mot de passe.";
-      print(_errorMessage);
+      setError("Une erreur s'est produite lors de la réinitialisation du mot de passe.");
+      print(error);
     } finally {
-      _isLoading = false;
-      notifyListeners();
+      setBusy(false); // Désactive le mode "chargement"
     }
   }
 }
