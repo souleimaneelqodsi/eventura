b3e37e5bf0b431af7fd4a6aa87bafa04387f51cc
souleimaneelqodsi <souleimane.el-qodsi@universite-paris-saclay.fr>
2025-03-11 17:15:24 +0100

    Simplified AuthWrapper as it is built only once at app startup, it needs only synchronous single verification of the auth status to redirect the user either to the home page or the sign up page.

--- lib/ui/widgets/auth_wrapper.dart
+++ lib/ui/widgets/auth_wrapper.dart
@@ -1,42 +1,20 @@
-import 'package:eventura/core/models/user.dart';
-import 'package:eventura/core/services/auth_service.dart';
 import 'package:eventura/ui/views/auth/signup_view.dart';
+import 'package:eventura/ui/views/homepage_view.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class AuthWrapper extends StatefulWidget {
-  final AuthService authService;
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
 
-  const AuthWrapper({super.key, required this.authService});
-
-  @override
-  State<AuthWrapper> createState() => _AuthWrapperState();
-}
-
-class _AuthWrapperState extends State<AuthWrapper> {
   @override
   Widget build(BuildContext context) {
-    if (widget.authService.currentUser != null) {
-      return FutureBuilder<UserModel?>(
-        future: widget.authService.getUserById(
-          widget.authService.currentUser!.id,
-        ),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (snapshot.hasData) {
-            bool firstLogin = snapshot.data!.firstLogin ?? false; 
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              Navigator.pushReplacementNamed(context, firstLogin ? '/welcome' : '/home');
-            });
-            return const SizedBox.shrink(); 
-          } else {
-            return Container(); 
-          }
-        },
-      );
+    Supabase.instance.client.auth.signOut();
+    final user = Supabase.instance.client.auth.currentUser;
+
+    if (user != null) {
+      return const HomepageView();
+    } else {
+      return SignupView();
     }
-    return SignupView();
   }
 }
