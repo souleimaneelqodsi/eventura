3f829feab9543f9aff8e53761579b338420465a7
Mac DEKHAIL <macdekhail@MacBook-Air-de-Mac.local>
2025-03-17 12:56:59 +0100

    Implement SettingsViewModel and SettingsView with local storage

--- lib/ui/views/settings_view.dart
+++ lib/ui/views/settings_view.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../core/viewmodels/settings_viewmodel.dart';
 
-class SettingsView extends StatelessWidget{
+class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Settings")),
+      body: Consumer<SettingsViewmodel>(
+        builder: (context, settingsViewModel, child) {
+          return ListView(
+            padding: const EdgeInsets.all(16.0),
+            children: [
+              SwitchListTile(
+                title: const Text("Dark Mode"),
+                value: settingsViewModel.isDarkMode,
+                onChanged: (value) {
+                  settingsViewModel.toggleDarkMode();
+                },
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
