328625c4e6ac43522743f348f51a6ad9bbde55d1
Mac DEKHAIL <macdekhail@MacBook-Air-de-Mac.local>
2025-04-11 02:04:38 +0200

    feat: implement dark mode toggle with persistent theme support

---
+++ lib/ui/views/auth/reset_password_view 2.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ResetPasswordView extends StatelessWidget{
+  const ResetPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
