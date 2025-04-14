6da5cef1047b81d75b8fda58ea1a386faad0a63a
souleimaneelqodsi <souleimane.el-qodsi@universite-paris-saclay.fr>
2025-04-14 06:11:47 +0200

    Fixed initialization error in the settings view for the settings viewmodel: loadSettings wasn't called + removed enable 'text/light' switching when tapping the switch in the view because it's confusing and not user-friendly.

--- lib/core/viewmodels/settings_viewmodel.dart
+++ lib/core/viewmodels/settings_viewmodel.dart
@@ -1,3 +1,24 @@
-import 'package:eventura/core/viewmodels/base_viewmodel.dart';
+import 'package:eventura/core/models/settings.dart';
+import 'package:eventura/core/services/settings_service.dart';
+import 'package:flutter/material.dart';
 
-class SettingsViewmodel extends BaseViewmodel{}
+class SettingsViewmodel extends ChangeNotifier {
+  final SettingsService _settingsService = SettingsService();
+
+  late Settings _settings;
+
+  Settings get settings => _settings;
+
+  Future<void> loadSettings() async {
+    _settings = await _settingsService.getAllSettings();
+    notifyListeners();
+  }
+
+  Future<void> toggleDarkMode() async {
+    final updatedSettings = await _settingsService.toggleLightMode();
+    if (updatedSettings != null) {
+      _settings = updatedSettings;
+      notifyListeners();
+    }
+  }
+}
--- lib/ui/views/settings_view.dart
+++ lib/ui/views/settings_view.dart
@@ -1,11 +1,35 @@
+import 'package:eventura/core/viewmodels/settings_viewmodel.dart';
 import 'package:flutter/material.dart';
-import 'package:eventura/ui/static/coming_soon.dart';
+import 'package:provider/provider.dart';
 
 class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ComingSoon(title: 'Settings');
+    final viewModel = Provider.of<SettingsViewmodel>(context);
+    viewModel.loadSettings();
+
+    return Consumer<SettingsViewmodel>(
+      builder:
+          (context, viewModel, child) => Scaffold(
+            appBar: AppBar(title: Text('Settings')),
+            body: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Enable Dark Mode", style: TextStyle(fontSize: 18)),
+                  Switch(
+                    value: !viewModel.settings.lightMode,
+                    onChanged: (_) {
+                      viewModel.toggleDarkMode();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+    );
   }
 }
